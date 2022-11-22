fb7081594b4f46e78709a5c4186535a98e9b5160
E-NoR <enorenor@gmail.com>
2022-11-23 02:48:39 +0800

    uvuv

--- Leetcode/279. Perfect Squares_med.py
+++ Leetcode/279. Perfect Squares_med.py
@@ -2,7 +2,7 @@
 # @Author: E-NoR
 # @Date:   2022-11-23 01:58:15
 # @Last Modified by:   E-NoR
-# @Last Modified time: 2022-11-23 02:42:51
+# @Last Modified time: 2022-11-23 02:48:12
 # @url: https://leetcode.com/problems/perfect-squares/submissions/
 
 class Solution:
@@ -12,8 +12,8 @@ class Solution:
         for j in range(t+1):
             if int((ms:=n - j * j)**0.5)**2 == ms:
                 return 2
-        while n % 4 == 0:
+        while not n & 0b11: #n % 4 == 0
             n >>= 2
-        if n % 8 == 7:
+        if (n & 0b111) == 0b111: #n % 8 == 7
             return 4
         return 3
