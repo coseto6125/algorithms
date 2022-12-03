2039180c1a8dadcf1b64539ec21ed67e31103b87
E-NoR <enorenor@gmail.com>
2022-12-04 03:50:26 +0800

    fix try

--- Leetcode/65. Valid Number_hard.py
+++ Leetcode/65. Valid Number_hard.py
@@ -2,12 +2,12 @@
 # @Author: E-NoR
 # @Date:   2022-11-18 23:48:35
 # @Last Modified by:   E-NoR
-# @Last Modified time: 2022-11-18 23:48:48
+# @Last Modified time: 2022-12-04 03:50:07
 # @url: https://leetcode.com/problems/valid-number/description/
 class Solution:
     def isNumber(self, s: str) -> bool:
-        try: 
+        try:
             float(s)
             return s if 'nf' not in s else False
-        except:
+        except Exception:
             return False
